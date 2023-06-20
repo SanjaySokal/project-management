bca67359d53c4acd2ac0fb4b9a7822f7ea2e4722
SanjaySokal <sokalsanjay@gmail.com>
2023-06-20 10:46:26 +0530

    working opn drag & drop

--- frontend/src/Components/ItemStatus.jsx
+++ frontend/src/Components/ItemStatus.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
+import { Draggable } from 'react-beautiful-dnd'
 
 const ItemStatus = (props) => {
     return (
-        <li>
-            <div className="top-btns">
-                <p className={`priority ${props.priority}`}>
-                    {props.priority.charAt(0).toUpperCase() + props.priority.slice(1)}
-                </p>
-                <span className="link"><i className="fa-solid fa-ellipsis"></i></span>
-            </div>
-            <div className="inner-content">
-                <h4>{props.heading}</h4>
-                <p>
-                    {props.para}
-                </p>
-            </div>
-        </li>
+        <Draggable>
+            {(provided) => {
+                <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                    <div className="top-btns">
+                        <p className={`priority ${props.priority}`}>
+                            {props.priority.charAt(0).toUpperCase() + props.priority.slice(1)}
+                        </p>
+                        <span className="link"><i className="fa-solid fa-ellipsis"></i></span>
+                    </div>
+                    <div className="inner-content">
+                        <h4>{props.heading}</h4>
+                        <p>
+                            {props.para}
+                        </p>
+                    </div>
+                </li>
+            }}
+        </Draggable>
     )
 }
 
--- frontend/src/Components/List.jsx
+++ frontend/src/Components/List.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { DragDropContext, Droppable } from "react-beautiful-dnd"
 import ToDo from './ToDo'
 import OnProgress from './OnProgress'
 import Completed from './Completed'
@@ -6,9 +7,35 @@ import Completed from './Completed'
 const List = () => {
     return (
         <div className="list">
-            <ToDo />
-            <OnProgress />
-            <Completed />
+            <DragDropContext onDragEnd={() => { }}>
+                <Droppable droppableId='todo'>
+                    {
+                        (provided) => {
+                            <div ref={provided.innerRef} {...provided.droppableProps}>
+                                <ToDo />
+                            </div>
+                        }
+                    }
+                </Droppable>
+                <Droppable droppableId='progress'>
+                    {
+                        (provided) => {
+                            <div ref={provided.innerRef} {...provided.droppableProps}>
+                                <OnProgress />
+                            </div>
+                        }
+                    }
+                </Droppable>
+                <Droppable droppableId='completed'>
+                    {
+                        (provided) => {
+                            <div ref={provided.innerRef} {...provided.droppableProps}>
+                                <Completed />
+                            </div>
+                        }
+                    }
+                </Droppable>
+            </DragDropContext>
         </div>
     )
 }
